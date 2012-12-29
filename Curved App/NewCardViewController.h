2c717b52cd357068e80a2c0f8a8f0e84c550345e
Charles Burgess <cvburgess@gmx.com>
2012-12-29 10:10:14 -0500

    Connected all VCs with appropriate classes Created NewCard screen

---
+++ Curved App/NewCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewCardViewController.h
+//  Curved App
+//
+//  Created by Charles Burgess on 12/29/12.
+//  Copyright (c) 2012 Createch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewCardViewController : UIViewController
+
+@end
